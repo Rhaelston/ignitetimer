ce230ab339c69fe0a0b401b1dbd95d0c1e146bd7
Rhaelston Honório <rhaelston@hotmail.com>
2022-10-04 14:57:26 -0300

    start

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,59 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórioco</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa1</td>
+              <td>10 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>tarefa1</td>
+              <td>10 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>tarefa1</td>
+              <td>10 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>tarefa1</td>
+              <td>10 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
